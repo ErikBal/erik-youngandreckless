9afd4b061cdbb2b49aac711f8c797cddd2002c3b
Erik Balonier <erikibali@gmail.com>
2015-03-14 14:15:52 -0500

    basic structur

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Erik-YoungandReckless
+		</title>
+		<link href="css/load.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<header>
+			<div id="top-nav" class="box">
+				<nav>
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</nav>
+			</div>
+			<div id="middle" class="box">
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+			<div id="bottom-nav" class="box">
+				<nav>
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<main>
+			<div id="top" class="box">
+				<img src="">
+			</div>
+			<div id="products" class="box">
+			</div>
+			<div id="bottom" class="box">
+
+			</div>
+		</main>
+	</body>
+</html>
