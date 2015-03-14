627553c5683347ac11076e0f0031e73ea488409b
Erik Balonier <erikibali@gmail.com>
2015-03-14 17:43:23 -0500

    second row header

--- index.php
+++ index.php
@@ -7,58 +7,63 @@
 		<link href="css/stylesheet.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
-		<header>
-			<section id="top-nav" class="box">
-				<nav>
-					<ul>
-						<li>
-							<a href="#">Reckless Points</a>
-						</li>
-					</ul>
-					<ul>
-						<li>
-							<img src="http://cdn.shopify.com/s/files/1/0340/3381/t/17/assets/i-currency-4.png?16579107724765185310">
-							<a href="#">USD</a>
-						</li>
-						<li>
-							<a href="#">Register</a>
-						</li>
-						<li>
-							<a href="#">Login</a>
-						</li>
-					</ul>
-				</nav>
-			</section>
-			<section id="middle" class="box">
-				<div></div>
-				<div></div>
-				<div></div>
-			</div>
-			<section id="bottom-nav" class="box">
-				<nav>
-					<ul>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-					</ul>
-				</nav>
-			</section>
-		</header>
-		<main>
-			<section id="top" class="box">
-				<img src="">
-			</section>
-			<section id="products" class="box">
+		<section>
+			<header>
+				<section id="top-nav" class="box">
+					<nav>
+						<ul>
+							<li>
+								<a href="#">Reckless Points</a>
+							</li>
+						</ul>
+						<ul>
+							<li>
+								<img src="http://cdn.shopify.com/s/files/1/0340/3381/t/17/assets/i-currency-4.png?16579107724765185310">
+								<a href="#">USD</a>
+							</li>
+							<li>
+								<a href="#">Register</a>
+							</li>
+							<li>
+								<a href="#">Login</a>
+							</li>
+						</ul>
+					</nav>
+				</section>
+				<section id="middle" class="box">
+					<div>fgdf</div>
+					<div></div>
+					<div>
+							<a href="#">MY CART (0)</a>
+					</div>
+				</div>
+				</section>
+				<section id="bottom-nav" class="box">
+					<nav>
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+							<li></li>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</nav>
+				</section>
+			</header>
+			<main>
+				<section id="top" class="box">
+					<img src="">
+				</section>
+				<section id="products" class="box">
 
-			</section>
-			<section id="bottom" class="box">
+				</section>
+				<section id="bottom" class="box">
 
-			</sections>
-		</main>
+				</sections>
+			</main>
+		</section>
 		<footer>
 			<section id="top">
 				<div>
