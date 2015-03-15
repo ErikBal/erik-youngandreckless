c069a7fa2296ed76fdb8edda39dfc75e5d8e343b
Erik Balonier <erikibali@gmail.com>
2015-03-15 12:43:02 -0500

    header finished so far

--- index.php
+++ index.php
@@ -7,65 +7,79 @@
 		<link href="css/stylesheet.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
-		<section>
-			<header>
-				<section id="top-nav" class="box">
-					<nav>
-						<ul>
-							<li>
-								<a href="#">Reckless Points</a>
-							</li>
-						</ul>
-						<ul>
-							<li>
-								<img src="http://cdn.shopify.com/s/files/1/0340/3381/t/17/assets/i-currency-4.png?16579107724765185310">
-								<a href="#">USD</a>
-							</li>
-							<li>
-								<a href="#">Register</a>
-							</li>
-							<li>
-								<a href="#">Login</a>
-							</li>
-						</ul>
-					</nav>
-				</section>
-				<section id="middle" class="box">
-					<div>
-						<input type="text" placeholder="LOOK FOR SOMETHING">
-					</div>
-					<div></div>
-					<div>
-							<a href="#">MY CART (0)</a>
-					</div>
+		<header>
+			<section id="top-nav" class="box">
+				<nav>
+					<ul>
+						<li>
+							<a href="#">Reckless Points</a>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							<img src="http://cdn.shopify.com/s/files/1/0340/3381/t/17/assets/i-currency-4.png?16579107724765185310">
+							<a href="#">USD</a>
+						</li>
+						<li>
+							<a href="#">Register</a>
+						</li>
+						<li>
+							<a href="#">Login</a>
+						</li>
+					</ul>
+				</nav>
+			</section>
+			<section id="middle" class="box">
+				<div>
+					<input type="text" placeholder="LOOK FOR SOMETHING">
 				</div>
-				</section>
-				<section id="bottom-nav" class="box">
-					<nav>
-						<ul>
-							<li></li>
-							<li></li>
-							<li></li>
-							<li></li>
-							<li></li>
-							<li></li>
-							<li></li>
-						</ul>
-					</nav>
-				</section>
-			</header>
-			<main>
-				<section id="top" class="box">
-					<img src="">
-				</section>
-				<section id="products" class="box">
+				<div>
+					<!--LOGO-->
+				</div>
+				<div>
+					<a href="#">MY CART (0)</a>
+				</div>
+				</div>
+			</section>
+			<section id="bottom-nav" class="box">
+				<nav>
+					<ul>
+						<li>
+							<a href="#">JUST IN</a>
+						</li>
+						<li>
+							<a href="#">GUYS</a>
+						</li>
+						<li>
+							<a href="#">GIRLS</a>
+						</li>
+						<li>
+							<a href="#">ACCESSORIES</a>
+						</li>
+						<li>
+							<a href="#">SOCIAL</a>
+						</li>
+						<li>
+							<a href="#">NEWS</a>
+						</li>
+						<li>
+							<a href="#">CONTACT US</a>
+						</li>
+					</ul>
+				</nav>
+			</section>
+		</header>
+		<main>
+			<section id="top" class="box">
+				<img src="">
+			</section>
+			<section id="products" class="box">
 
-				</section>
-				<section id="bottom" class="box">
+			</section>
+			<section id="bottom" class="box">
 
-				</sections>
-			</main>
-		</section>
+			</sections>
+		</main>
 		<footer>
 			<section id="top">
 				<div>
@@ -75,8 +89,8 @@
 			</section>
 			<section id="middle">
 				<div>
-				</div>
 			</div>
+				</section>
 			<section id="bottom">
 				<article>
 				</article>
