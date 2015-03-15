5b27d699996c7feb392f1b3353403d6e86de4c76
Erik Balonier <erikibali@gmail.com>
2015-03-15 14:52:29 -0500

    basic structure of products finished

--- index.php
+++ index.php
@@ -71,10 +71,78 @@
 		</header>
 		<main>
 			<section id="top" class="box">
-				<img src="">
+				<img src="http://cdn.shopify.com/s/files/1/0340/3381/t/17/assets/slideshow_2.jpg?16579107724765185310">
 			</section>
 			<section id="products" class="box">
-
+				<h2>NEW PRODUCTS</h2>
+				<!--FIRST ROW-->
+				<figure>
+					<span id="img1" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img2" class="img-animation" ></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img3" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img4" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<!--SECOND ROW-->
+				<figure>
+					<span id="img5" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img6" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img7" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img8" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<!--THIRD ROW-->
+				<figure>
+					<span id="img9" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img10" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img11" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img12" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<!--FORTH ROW-->
+				<figure>
+					<span id="img13" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img14" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img15" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
+				<figure>
+					<span id="img16" class="img-animation"></span>
+					<figcaption> </figcaption>
+				</figure>
 			</section>
 			<section id="bottom" class="box">
 
